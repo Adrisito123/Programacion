b9fa086622a6296e9b5e874e78f5bf0f517633f7
Adrisito123 <131748443+Adrisito123@users.noreply.github.com>
2024-11-28 10:52:21 +0100

    Add files via upload

--- Programación/UD3/Ejercicio1-2/Ejercicio2.java
+++ Programación/UD3/Ejercicio1-2/Ejercicio2.java
@@ -1,22 +1,30 @@
 package Ejercicios;
 
 public class Ejercicio2 {
-	public static void main(String[]args) {
-		
-		int valor =  10;
-		cambiarValor(valor);
-		System.out.println("Despues del metodo: " + valor);
-		
-		int[] array = {10};
-		cambiarArray(array);
-		System.out.println("Despues del metodo: " + array[0]);
-	
-	}
-	public static void cambiarValor(int numero) {
-		numero = 20;
-	}
-	public static void cambiarArray(int[]array) {
-		array[0] = 20;
-	}
-}
+    public static void main(String[] args) {
+        
+        int[] entero = new int[10];
+        
+        entero[0] = 1;
+        entero[1] = 2;
+        entero[2] = 3;
+        entero[3] = 4;
+        entero[4] = 5;
+        entero[5] = 6;
+        entero[6] = 7;
+        entero[7] = 8;
+        entero[8] = 9;
+        entero[9] = 10;
 
+        System.out.println("Elemento en la posición 0: " + entero[0]);
+        System.out.println("Elemento en la posición 1: " + entero[1]);
+        System.out.println("Elemento en la posición 2: " + entero[2]);
+        System.out.println("Elemento en la posición 3: " + entero[3]);
+        System.out.println("Elemento en la posición 4: " + entero[4]);
+        System.out.println("Elemento en la posición 5: " + entero[5]);
+        System.out.println("Elemento en la posición 6: " + entero[6]);
+        System.out.println("Elemento en la posición 7: " + entero[7]);
+        System.out.println("Elemento en la posición 8: " + entero[8]);
+        System.out.println("Elemento en la posición 9: " + entero[9]);
+    }
+}
