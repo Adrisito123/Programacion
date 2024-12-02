45e50d2ddb4a58fd14910f98a92d80121bda39f1
Adrisito123 <131748443+Adrisito123@users.noreply.github.com>
2024-12-02 12:36:04 +0100

    Add files via upload

---
+++ Programación/UD3/Ejercicio1-2/Ejercicio2.java
@@ -0,0 +1,30 @@
+package Ejercicios;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        
+        int[] entero = new int[10];
+        
+        entero[0] = 1;
+        entero[1] = 2;
+        entero[2] = 3;
+        entero[3] = 4;
+        entero[4] = 5;
+        entero[5] = 6;
+        entero[6] = 7;
+        entero[7] = 8;
+        entero[8] = 9;
+        entero[9] = 10;
+
+        System.out.println("Elemento en la posición 0: " + entero[0]);
+        System.out.println("Elemento en la posición 1: " + entero[1]);
+        System.out.println("Elemento en la posición 2: " + entero[2]);
+        System.out.println("Elemento en la posición 3: " + entero[3]);
+        System.out.println("Elemento en la posición 4: " + entero[4]);
+        System.out.println("Elemento en la posición 5: " + entero[5]);
+        System.out.println("Elemento en la posición 6: " + entero[6]);
+        System.out.println("Elemento en la posición 7: " + entero[7]);
+        System.out.println("Elemento en la posición 8: " + entero[8]);
+        System.out.println("Elemento en la posición 9: " + entero[9]);
+    }
+}
